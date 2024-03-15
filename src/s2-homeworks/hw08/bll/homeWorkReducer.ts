1cfae74b171e78526075e31df91685cc2a6be18b
Yan Yu <bx6xbChosen1@gmail.com>
2024-03-15 21:44:51 +0600

    hw8 is ready

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,26 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8"
 
-type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+type ActionType = { type: "sort"; payload: "up" | "down" } | { type: "check"; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case "sort": {
+      const sortedCopy = state.slice().sort((a, b) => {
+        if (a.name > b.name) {
+          return 1
         }
-        case 'check': {
-
-            return state // need to fix
+        if (a.name < b.name) {
+          return -1
         }
-        default:
-            return state
+        return 0
+      })
+      return action.payload === "up" ? sortedCopy : sortedCopy.reverse()
     }
+    case "check": {
+      return state.filter((u) => u.age > 18)
+    }
+    default:
+      return state
+  }
 }
