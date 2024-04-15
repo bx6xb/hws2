04efae0238b9e82e87be5adb80fd4a726bb006c1
Yan Yu <bx6xbChosen1@gmail.com>
2024-04-15 21:37:39 +0600

    hw10 is ready

--- src/s2-homeworks/hw05/header/Header.tsx
+++ src/s2-homeworks/hw05/header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react"
+import { FC } from "react"
 import burgerIcon from "./burger.svg"
 import s from "./Header.module.css"
 import { useLocation } from "react-router-dom"
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,22 +1,29 @@
 const initState = {
-    isLoading: false,
+  isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // пишет студент  // need to fix
+type LoadingStateType = typeof initState
 
-        default:
-            return state
-    }
+type LoadingReducerActionType = LoadingActionType
+
+export const loadingReducer = (
+  state: LoadingStateType = initState,
+  action: LoadingReducerActionType
+): LoadingStateType => {
+  // fix any
+  switch (action.type) {
+    // пишет студент  // need to fix
+    case "CHANGE_LOADING":
+      return { ...state, isLoading: action.isLoading }
+    default:
+      return state
+  }
 }
 
-type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
-}
+type LoadingActionType = ReturnType<typeof loadingAC>
 
-export const loadingAC = (isLoading: boolean): LoadingActionType => ({
-    type: 'CHANGE_LOADING',
+export const loadingAC = (isLoading: boolean) =>
+  ({
+    type: "CHANGE_LOADING",
     isLoading,
-})
+  } as const)
