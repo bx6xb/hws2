8308bd2e12cd0e16e0b07bda3f7e3b5ca8dac28a
Yan Yu <bx6xbChosen1@gmail.com>
2024-04-23 21:56:30 +0600

    hw11 is ready

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import {Slider, SliderProps} from '@mui/material'
+import React from "react"
+import { Slider, SliderProps } from "@mui/material"
 
 const SuperRange: React.FC<SliderProps> = (props) => {
-    return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
-        />
-    )
+  const greenColor = "#00CC22"
+
+  return (
+    <Slider
+      sx={{
+        // стили для слайдера // пишет студент
+        color: greenColor,
+        width: "170px",
+        "& .MuiSlider-thumb": {
+          border: `1px solid ${greenColor}`,
+          bgcolor: "white",
+        },
+      }}
+      {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+    />
+  )
 }
 
 export default SuperRange
